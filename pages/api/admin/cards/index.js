5e14ed3cdfc2497f9b34fd1365355535baf41edc
Ilya Maslikov <ilyamaslikov@Noutbuk-Ilya.local>
2025-04-04 20:01:52 +0700

    Исправлена обработка полей с запятыми и источников трафика

--- pages/api/admin/cards/index.js
+++ pages/api/admin/cards/index.js
@@ -53,11 +53,12 @@ export default async function handler(req, res) {
           budgetValue: parseInt(req.body.budget, 10),
           experience: req.body.type === 'supplier' ? parseInt(req.body.experience, 10) : null,
           foundedYear: req.body.type === 'advertiser' ? parseInt(req.body.foundedYear, 10) : null,
-          trafficSource: req.body.type === 'supplier' ? req.body.trafficSource : '',
-          sources: req.body.type === 'advertiser' ? (Array.isArray(req.body.sources) ? req.body.sources : []) : [],
-          goals: Array.isArray(req.body.goals) ? req.body.goals : [],
-          advantages: Array.isArray(req.body.advantages) ? req.body.advantages : [],
-          features: [],
+          trafficSource: req.body.type === 'supplier' ? (req.body.trafficSource || '') : '',
+          sources: req.body.type === 'advertiser' ? 
+            (Array.isArray(req.body.sources) ? req.body.sources : [req.body.sources].filter(Boolean)) : [],
+          goals: Array.isArray(req.body.goals) ? req.body.goals : [req.body.goals].filter(Boolean),
+          advantages: Array.isArray(req.body.advantages) ? req.body.advantages : [req.body.advantages].filter(Boolean),
+          features: Array.isArray(req.body.features) ? req.body.features : [],
           clicks: 0
         }
       });
