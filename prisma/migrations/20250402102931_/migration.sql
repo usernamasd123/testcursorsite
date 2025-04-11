b373298ab03fa0392635c4f480eae816d8dfe693
Ilya Maslikov <ilyamaslikov@Noutbuk-Ilya.local>
2025-04-11 21:48:38 +0700

    Обновлена схема базы данных: удалено поле sources, оставлена только связь через trafficSources

--- prisma/migrations/20250402102931_/migration.sql
+++
@@ -1,5 +0,0 @@
--- Переименовываем ограничение первичного ключа
-ALTER TABLE "Message" RENAME CONSTRAINT "Message_new_pkey" TO "Message_pkey";
-
--- Добавляем колонку isBot
-ALTER TABLE "Message" ADD COLUMN "isBot" BOOLEAN NOT NULL DEFAULT false;
